334cc5c073cb1b2cb6fab4f3dd0b4e623f2941a6
ankur1610 <vora.a@husky.neu.edu>
2017-08-20 01:15:21 -0400

    Controller beginning

---
+++ src/main/java/com/savingstar/application/SavingStarApiApp.java
@@ -0,0 +1,15 @@
+package com.savingstar.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingStarApiApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SavingStarApiApp.class, args);
+
+	}
+
+}
